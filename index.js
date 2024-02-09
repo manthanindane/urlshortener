601a611d34d49fcdc9bf35775bc74c275c6b2789
vrushbhakshirsagar <manthanindane141@gmail.com>
2024-02-09 11:13:15 +0530

    new commit

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ app.use(express.json());
 
 
 app.get("/", (req, res) => {
-app.use(express.static(path.resolve(__dirname, "build")));
-res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+app.use(express.static(path.resolve(__dirname,"frontend", "build")));
+res.sendFile(path.resolve(__dirname,  "frontend", "build", "index.html"));
 });
 
 app.use((req, res, next) => {
